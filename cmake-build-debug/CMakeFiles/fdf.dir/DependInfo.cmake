637c180f1561dfd8df86df5df7ad029d7efb6ff9
arz <ayaz6ay@gmail.com>
2020-05-04 16:37:41 +0300

    one commit

---
+++ cmake-build-debug/CMakeFiles/fdf.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/arz/projects/fdf/create_matrix.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/create_matrix.c.o"
+  "/home/arz/projects/fdf/f_free.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/f_free.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_atoi.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_atoi.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_bzero.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_bzero.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_isalnum.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_isalnum.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_isalpha.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_isalpha.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_isascii.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_isascii.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_isdigit.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_isdigit.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_isprint.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_isprint.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_itoa.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_itoa.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lst_contentsearch.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lst_contentsearch.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lst_size.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lst_size.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstadd.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstadd.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstdel.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstdel.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstdelone.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstdelone.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstiter.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstiter.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstmap.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstmap.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstnew.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstnew.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_lstnpoint.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_lstnpoint.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memalloc.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memalloc.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memccpy.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memccpy.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memchr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memchr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memcmp.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memcmp.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memcpy.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memcpy.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memdel.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memdel.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memmove.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memmove.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_memset.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_memset.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putchar.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putchar.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putchar_fd.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putchar_fd.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putendl.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putendl.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putendl_fd.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putendl_fd.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putnbr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putnbr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putnbr_fd.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putnbr_fd.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putstr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putstr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_putstr_fd.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_putstr_fd.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_sort_wordtab.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_sort_wordtab.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_str_reverse.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_str_reverse.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strcat.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strcat.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strchr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strchr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strclr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strclr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strcmp.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strcmp.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strcpy.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strcpy.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strdel.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strdel.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strdup.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strdup.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strequ.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strequ.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_striter.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_striter.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_striteri.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_striteri.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strjoin.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strjoin.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strlcat.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strlcat.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strlen.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strlen.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strlowcase.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strlowcase.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strmap.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strmap.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strmapi.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strmapi.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strncat.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strncat.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strncmp.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strncmp.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strncpy.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strncpy.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strnequ.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strnequ.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strnew.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strnew.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strnstr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strnstr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strrchr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strrchr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strsplit.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strsplit.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strstr.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strstr.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strsub.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strsub.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strtrim.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strtrim.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_strupcase.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_strupcase.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_tolower.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_tolower.c.o"
+  "/home/arz/projects/fdf/libft/srcs/ft_toupper.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/ft_toupper.c.o"
+  "/home/arz/projects/fdf/libft/srcs/get_next_line.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/srcs/get_next_line.c.o"
+  "/home/arz/projects/fdf/main.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/main.c.o"
+  "/home/arz/projects/fdf/read_file.c" "/home/arz/projects/fdf/cmake-build-debug/CMakeFiles/fdf.dir/read_file.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../includes"
+  "../libft/includes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
